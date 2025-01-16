c2adadaac66a51a4fd2fd0b0f0341732538509ac
YoshidaTomoaki <ydaydada.work@gmail.com>
2025-01-16 19:45:57 +0900

    sample page

---
+++ app/routes/_app.sample/route.tsx
@@ -0,0 +1,16 @@
+import { LoaderFunctionArgs } from "@remix-run/cloudflare";
+
+export async function loader({ request, context }: LoaderFunctionArgs) {
+  return null;
+}
+
+export default function Sample() {
+  throw new Error("サンプルエラー");
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full text-white">
+      <h1 className="text-4xl font-bold mb-6">PokeAPI GraphQL Practice</h1>
+      <p className="text-xl mb-8">サンプルページ</p>
+    </div>
+  );
+}
